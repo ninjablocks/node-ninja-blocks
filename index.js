f0382855cddd9f70b50e30f2ccdf4d2db05e59a9
Daniel Friedman <danielnfriedman@gmail.com>
2012-09-07 11:45:39 +1000

    More user functions

--- index.js
+++ index.js
@@ -3,15 +3,109 @@ var uri           = 'https://api.ninja.is/rest/v0/';
 
 
 exports.app = function(opts) {
+  
   var access_token = opts.access_token;
+
   return {
+    /**
+     * Fetched all the user's information. If no callback is provided
+     * then more specific information can be requested
+     *
+     * Example:
+     *     app.user(function(err, data) { ... })
+     *     app.user().stream(function(err,data){ ... })
+     *
+     * @param {Function} cb
+     * @api public
+     */
+    user: function(cb) {
+      if (typeof cb === "function") {
+        var opts = {
+          url: uri + 'user',
+          method: 'GET',
+          qs: { access_token:access_token },
+          json: true
+        };
+        request(opts,function(e,r,b) {
+          if (e) cb(e)
+          else {
+            if (r.statusCode==200) cb(null, b)
+            else {
+              cb({statusCode:r.statusCode,error:b.error})
+            }
+          }
+        });        
+        return;
+      }
+
+      return {
+        /**
+         * Fetched the user's stream
+         *
+         * Example:
+         *     app.user().stream(function(err,data){ ... })
+         *
+         * @param {Function} cb
+         * @api public
+         */
+        stream: function(cb) {
+          var opts = {
+            url: uri + 'user/stream',
+            method: 'GET',
+            qs: { access_token:access_token },
+            json: true
+          };
+          request(opts,function(e,r,b) {
+            if (e) cb(e)
+            else {
+              if (r.statusCode==200) cb(null, b)
+              else {
+                cb({statusCode:r.statusCode,error:b.error})
+              }
+            }
+          });  
+        },
+        /**
+         * Fetches the user's pucher channel
+         *
+         * Example:
+         *     app.user().pusher_channel(function(err,data){ ... })
+         *
+         * @param {Function} cb
+         * @api public
+         */
+        pusher_channel: function(cb) {
+          var opts = {
+            url: uri + 'user/pusherchannel',
+            method: 'GET',
+            qs: { access_token:access_token },
+            json: true
+          };
+          request(opts,function(e,r,b) {
+            if (e) cb(e)
+            else {
+              if (r.statusCode==200) cb(null, b.data)
+              else {
+                cb({statusCode:b.id||200,error:b.error})
+              }
+            }
+          });  
+        },
+      }
+    },
 
     device: function(device) {
 
       return {
-
-        /*
-            Actuate a device
+        /**
+         * Actuates a device, sending a given `command`.
+         *
+         * Example:
+         *     app.device(a_led_guid).actuate('FFFFFF',function(err) { ... })
+         *
+         * @param {String} command
+         * @param {Function} cb
+         * @api public
          */
         actuate: function(command,cb) {
           var opts = {
@@ -30,21 +124,21 @@ exports.app = function(opts) {
         },
 
         /**
-         * Subscribe to the device's data feed
-         * Optionally `overwrite` an existing callback url with the provided
-         * `url`. Default is false.
+         * Subscribes to a device's data feed.
+         * 
+         * Optionally `overwrite`s an existing callback `url`
+         * Default is false.
          *
          * Example:
-         *     app.device(guid).subscribe('example.com',true)
+         *     app.device(guid).subscribe('example.com',true,function(err) { ... })
          *
-         * @param {String} url
-         * @param {Boolean} overwrite
+         * @param {String} url The url that Ninja Blocks will POST data to
+         * @param {Boolean} overwrite If a callback url exists, this flag will force an overwrite
          * @param {Function} cb
          * @api public
          */
-
         subscribe: function(url,overwrite,cb) {
-          if (typeof overwrite==="function") {
+          if (typeof overwrite === "function") {
             cb = overwrite;
             overwrite = false;
           }
@@ -81,24 +175,60 @@ exports.app = function(opts) {
           });
         },
 
-        /*
-            Fetch historical data
+        /**
+         * Unubscribes to a device's data feed.
+         * 
+         * Example:
+         *     app.device(guid).unsubscribe(function(err) { ... })
+         *
+         * @param {Function} cb
+         * @api public
          */
-        
-        data: function(from, to, cb) {
+        unsubscribe: function(cb) {
+          var opts = {
+            url: uri + 'device/'+device+'/callback',
+            method: 'DELETE',
+            qs: { access_token:access_token }
+          };
+          request(opts,function(e,r,b) {
+            if (e) cb(e)
+            else {
+              if (b.result===1) cb(null)
+              else {
+                cb({statusCode:b.id||200,error:b.error})
+              }
+            }
+          });
+        },
+
+        /**
+         * Fetches any historical data about this device.
+         * Optionally specify the period's `start` and `end`
+         * timestamp.
+         *
+         * Example:
+         *     app.device(guid).data(start, end, function(err, data) { ... })
+         *
+         * @param {Integer|Date} start The timestamp/datetime of the beginning of the period
+         * @param {Integer|Date} end the timestamp/datetime of the end of the period
+         * @param {Function} cb
+         * @api public
+         */
+        data: function(start, end, cb) {
           var qs = {access_token:access_token};
 
-          if (typeof from === "function") {
-            cb = from;
-            from = false;
-          } else if (typeof to === "function") {
-            cb = to;
-            to = false;
+          if (typeof start === "function") {
+            cb = start;
+            start = false;
+          } else if (typeof end === "function") {
+            cb = end;
+            end = false;
           }
-          if (from instanceof Date) from = from.getTime();
-          if (from) qs.from = from;
-          if (to instanceof Date) to = to.getTime();
-          if (to) qs.to = to;
+
+          if (start instanceof Date) start = start.getTime();
+          if (start) qs.from = start;
+          if (end instanceof Date) end = end.getTime();
+          if (end) qs.to = end;
 
           var opts = {
             url: uri + 'device/'+device+'/data',
@@ -108,7 +238,6 @@ exports.app = function(opts) {
           };
 
           request(opts,function(e,r,b) {
-            console.dir(b);
             if (e) cb(e)
             else {
               if (b.result===1) cb(null, b.data)
@@ -117,10 +246,15 @@ exports.app = function(opts) {
           });
         },
 
-        /*
-            Fetch latest data
+        /**
+         * Fetches the last heartbeat received by this device.
+         *
+         * Example:
+         *     app.device(guid).last_heartbeat(function(err, data) { ... })
+         *
+         * @param {Function} cb
+         * @api public
          */
-        
         last_heartbeat: function(cb) {
           var opts = {
             url: uri + 'device/'+device+'/heartbeat',
@@ -140,7 +274,15 @@ exports.app = function(opts) {
       }
 
     },
-
+    /**
+     * Fetched all the user's device details
+     *
+     * Example:
+     *     app.devices(function(err, data) { ... })
+     *
+     * @param {Function} cb
+     * @api public
+     */
     devices: function(cb) {
       var opts = {
         url: uri + 'devices',
